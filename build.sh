b04647d8db58d6c8c49d4a303d4925a6d32aba83
hlgsdx <20733681+hlgsdx@users.noreply.github.com>
2023-09-19 16:38:14 +0800

    Update to LLVM 17.0.0

--- build.sh
+++ build.sh
@@ -1,22 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 LLVM_RELEASE="$(cat llvm-release.txt)"
+LLVM_RELEASE_RC="$(cat llvm-rc.txt)"
+
+if [ -z "$LLVM_RELEASE_RC" ]; then
+  LLVM_RELEASE_RC=""
+else
+  LLVM_RELEASE_RC="-rc$LLVM_RELEASE_RC"
+fi
+
 if [ "$1" != "stage1" ]; then
     tar -xf build_directory.tar
 fi
 cp final/llvm-project/llvm/utils/release/test-release.sh .
 patch -p1 < 0001-skip-tests.patch
 patch -p1 < 0002-support-partitioned-builds.patch
-./test-release.sh -release "$LLVM_RELEASE" -final -triple x86-64-apple-darwin21.0 -no-checkout -no-clang-tools -no-test-suite -no-openmp -no-polly -no-mlir -no-flang -use-gzip -no-compare-files -configure-flags -DLLVM_APPEND_VC_REV=OFF -"$1"
+./test-release.sh -release "$LLVM_RELEASE" -final -triple x86-64-apple-darwin22.0 -no-checkout -no-clang-tools -no-test-suite -no-openmp -no-polly -no-mlir -no-flang -use-gzip -no-compare-files -configure-flags -DLLVM_APPEND_VC_REV=OFF -"$1"
 if [ "$1" != "stage3" ]; then
     tar -cf build_directory.tar final
     exit 0
 fi
-_release_tag_version="$LLVM_RELEASE"
+_release_tag_version="$LLVM_RELEASE""$LLVM_RELEASE_RC"
 [ "$(cat revision.txt)" -ne 0 ] && _release_tag_version="$_release_tag_version"-"$(cat revision.txt)"
-echo "file_name=clang+llvm-$LLVM_RELEASE-x86-64-apple-darwin21.0.tar.gz" >> $GITHUB_OUTPUT
+echo "file_name=clang+llvm-$LLVM_RELEASE-x86-64-apple-darwin22.0.tar.gz" >> $GITHUB_OUTPUT
 echo "release_tag_version=$_release_tag_version" >> $GITHUB_OUTPUT
 printf 'SHA512 checksum:\n<code>' > github_release_text.md
-printf "$(shasum -a 512 final/clang+llvm-"$LLVM_RELEASE"-x86-64-apple-darwin21.0.tar.gz | sed 's,final/,,' | sed 's, ,\&nbsp;,g')" >> github_release_text.md
+printf "$(shasum -a 512 final/clang+llvm-"$LLVM_RELEASE"-x86-64-apple-darwin22.0.tar.gz | sed 's,final/,,' | sed 's, ,\&nbsp;,g')" >> github_release_text.md
 printf '</code>\n' >> github_release_text.md
 mkdir output
-mv final/clang+llvm-"$LLVM_RELEASE"-x86-64-apple-darwin21.0.tar.gz output/
+mv final/clang+llvm-"$LLVM_RELEASE"-x86-64-apple-darwin22.0.tar.gz output/
--- initialize.sh
+++ initialize.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 LLVM_RELEASE="$(cat llvm-release.txt)"
-[ ! -f llvm-project-"$LLVM_RELEASE".src.tar.xz ] && curl -O -L https://github.com/llvm/llvm-project/releases/download/llvmorg-"$LLVM_RELEASE"/llvm-project-"$LLVM_RELEASE".src.tar.xz
-[ "$(shasum -a 512 llvm-project-"$LLVM_RELEASE".src.tar.xz)" != "$(cat checksum.txt)" ] && exit 1
+LLVM_RELEASE_RC="$(cat llvm-rc.txt)"
+
+if [ -z "$LLVM_RELEASE_RC" ]; then
+  LLVM_RELEASE_RC=""
+else
+  LLVM_RELEASE_RC="-rc$LLVM_RELEASE_RC"
+fi
+
+LLVM_TARBALL_FILENAME=llvm-project-"$LLVM_RELEASE""${LLVM_RELEASE_RC//-}".src
+[ ! -f llvm-project-"$LLVM_TARBALL_FILENAME".tar.xz ] && curl -O -L https://github.com/llvm/llvm-project/releases/download/llvmorg-"$LLVM_RELEASE""$LLVM_RELEASE_RC"/"$LLVM_TARBALL_FILENAME".tar.xz
+[ "$(shasum -a 512 "$LLVM_TARBALL_FILENAME".tar.xz)" != "$(cat checksum.txt)" ] && exit 1
 mkdir final
-tar -xf llvm-project-"$LLVM_RELEASE".src.tar.xz
-mv llvm-project-"$LLVM_RELEASE".src final/llvm-project
+tar -xf "$LLVM_TARBALL_FILENAME".tar.xz
+mv "$LLVM_TARBALL_FILENAME" final/llvm-project
